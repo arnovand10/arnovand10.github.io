9cbf357228e918b751cc4b8bc739a4d5b65bfb34
arnovand10 <arnovand10@student.arteveldehs.be>
2016-11-25 21:12:36 +0100

    core

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+var Utils = {
+	 store: function(namespace, data) {
+        if(arguments.length > 1) {
+            return localStorage.setItem(namespace, JSON.stringify(data));
+        } else {
+            var storedData = localStorage.getItem(namespace);
+            return (storedData && JSON.parse(storedData)) || null;
+        }
+ 	},
+ };
